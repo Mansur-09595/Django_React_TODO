5abec0f4b99c88906e1cbfd17476fd7e4bd302dc
Mansur-09595 <mansur_musaev_1997@mail.ru>
2023-04-10 07:22:27 +0300

    connect backend API to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as  Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,25 +8,50 @@ import TodosList from "./component/todo-list";
 import AddTodo from "./component/add-todo";
 import Login from "./component/login";
 import Signup from "./component/signup";
+import TodoDataService from "./services/todos";
 import "./App.css";
 
 function App() {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
   const [error, setError] = useState("");
+  console.log(user);
 
   const login = async (user = null) => {
     // default user to null
-    setUser(user);
+    TodoDataService.login(user)
+      .then((response) => {
+        setToken(response.data.token);
+        setUser(user.username);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", user.username);
+        setError("");
+      })
+      .catch((e) => {
+        console.log("login", e);
+        setError(e.toString());
+      });
   };
-  
+
   const logout = async () => {
-    setUser(null);
+    setToken("");
+    setUser("");
+    localStorage.setItem("token", "");
+    localStorage.setItem("user", "");
   };
-  
+
   const signup = async (user = null) => {
-    // default user to null
-    setUser(user);
+    TodoDataService.signup(user)
+      .then((response) => {
+        setToken(response.data.token);
+        setUser(user.username);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", user.username);
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(e.toString());
+      });
   };
 
   return (
@@ -40,7 +65,9 @@ function App() {
                 Todos
               </Link>
               {user ? (
-                <Link class="nav-link">Logout ({user})</Link>
+                <Link class="nav-link" onClick={logout}>
+                  Logout ({user})
+                </Link>
               ) : (
                 <>
                   <Link class="nav-link" to={"/login"}>
@@ -57,39 +84,46 @@ function App() {
       </Navbar>
 
       <div className="container mt-4">
-      <Routes>
-        <Route
-          path="/"
-          element={<TodosList token={token} />}
-        />
-        <Route
-          path="/todos"
-          element={<TodosList token={token} />}
-        />
-        <Route
-          path="/todos/create"
-          element={<AddTodo token={token} />}
-        />
-        <Route
-          path="/todos/:id"
-          element={<AddTodo token={token} />}
-        />
-        <Route
-          path="/login"
-          element={<Login login={login} />}
-        />
-        <Route
-          path="/signup"
-          element={<Signup signup={signup} />}
-        />
-      </Routes>
+        <div className="container mt-4">
+          <Routes>
+            <Route
+              exact
+              path={["/", "/todos"]}
+              render={(props) => <TodosList {...props} token={token} />}
+            ></Route>
+            <Route
+              path="/todos/create"
+              render={(props) => <AddTodo {...props} token={token} />}
+            ></Route>
+            <Route
+              path="/todos/:id/"
+              render={(props) => <AddTodo {...props} token={token} />}
+            ></Route>
+            <Route
+              path="/login"
+              render={(props) => <Login {...props} login={login} />}
+            ></Route>
+            <Route
+              path="/signup"
+              render={(props) => <Signup {...props} signup={signup} />}
+            ></Route>
+          </Routes>
+        </div>
         <footer
           className="text-center text-lg-start
         bg-light text-muted mt-4"
         >
           <div className="text-center p-4">
             © Copyright
-            <a target="_blank" className="text-reset fw-bold text-decoration-none" href="https://github.com/Mansur-09595" rel="noreferrer"> Mansur Musaev</a>
+            <a
+              target="_blank"
+              className="text-reset fw-bold text-decoration-none"
+              href="https://github.com/Mansur-09595"
+              rel="noreferrer"
+            >
+              {" "}
+              Mansur Musaev
+            </a>
           </div>
         </footer>
       </div>
